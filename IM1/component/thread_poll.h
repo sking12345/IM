d97a7202830a1d32f5d3b0e5345c6d3eaead6595
sking12345 <1281099078@qq.com>
2019-03-01 19:39:27 +0700

    u

--- IM1/component/thread_poll.h
+++ IM1/component/thread_poll.h
@@ -21,9 +21,34 @@ typedef struct thread_job
 typedef struct thread
 {
 	pthread_t    id;	//现场ID
-	int          shutdown;
+	int          queue_max_num;
 	struct thread_job *head;
-
+	pthread_mutex_t mutex;
+	pthread_cond_t queue_empty;
+	pthread_cond_t queue_not_empty;
+	pthread_cond_t queue_not_full;
+	int queue_cur_num;                //队列当前的job个数
+	int queue_close;                  //队列是否已经关闭
+	int pool_close;                   //线程池是否已经关闭
 } thread_t;
 
+typedef struct thread_poll   //线程池队列
+{
+	struct thread * thread;	//线程池
+	int thread_num;		//线程数
+} thread_poll_t;
+
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
